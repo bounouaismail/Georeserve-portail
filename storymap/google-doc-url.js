7b665212d1ed627ccfa7fbd09c539788834c28f5
bounouaismail <107546720+bounouaismail@users.noreply.github.com>
2022-11-23 17:54:22 +0100

    Add files via upload

---
+++ storymap/google-doc-url.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AAwVm8T3v737acJUqpk9PlCOlKYLXXrLmkeFutuiiFA/edit?usp=sharing';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyABiqQ9jJoZI2wy6xdt7Wngx7Vr65ndPjg';
